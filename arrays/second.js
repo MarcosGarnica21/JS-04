924834fee127f2bd5778444c3e105dc4f2cc60e3
MarcosGarnica21 <garnicar_@hotmail.com>
2025-06-12 14:08:57 -0600

    Add second exercise

---
+++ arrays/second.js
@@ -0,0 +1,23 @@
+/**
+ *Find the name of a secret society based on the first letter of each member's name.
+ 
+
+Examples:
+- `secretName(["Esperanza", "Franco", "Nia"])` should return `'EFN'`.
+- `secretName(['Phoebe', 'Ross', 'Chandler', 'Joey', 'Monica', 'Rachel'])` should return `'CJMPRR'`.
+- `secretName(['Harry', 'Ron', 'Hermione'])` should return `'HHR'`. 
+
+ */ 
+
+const secretName = function(namesArray){
+    let societyName = "";
+    const sortedNames = namesArray.toSorted();
+    sortedNames.forEach((name) => societyName = societyName + name[0]);
+    return societyName;   
+}
+console.log(secretName(["Esperanza", "Franco", "Nia"]));
+console.log(secretName(['Phoebe', 'Ross', 'Chandler', 'Joey', 'Monica', 'Rachel']));
+console.log(secretName(['Harry', 'Ron', 'Hermione']));
+
+
+
